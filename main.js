698f9cf01ba5c3b68a7e4cbb2d1763bd733ad10b
Yazima <yazima04200@gmail.com>
2023-07-06 17:32:11 +0900

    do... while文追加

--- main.js
+++ main.js
@@ -92,10 +92,21 @@
 // const command = Number(prompt('Menu 1, 2, 3 or 0 to exit'));
 // console.log(`Menu ${command} processed.`);
 
-let command = Number(prompt('Menu 1, 2, 3 or 0 to exit'));
+// let command = Number(prompt('Menu 1, 2, 3 or 0 to exit'));
 
-while (command !== 0) {
-  console.log(`Menu ${command} processed.`);
+// while (command !== 0) {
+//   console.log(`Menu ${command} processed.`);
+//   command = Number(prompt('Menu 1, 2, 3 or 0 to exit'));
+// }
+
+let command;
+
+do {
   command = Number(prompt('Menu 1, 2, 3 or 0 to exit'));
-}
+  if (command === 0) {
+    console.log('終了します');
+  } else {
+    console.log(`Menu ${command} processed.`);
+  }
+} while (command !== 0);
 
