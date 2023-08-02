8e017cd7ec25784b0118c4f39fbc0f0a20a1d5bf
Yazima <yazima04200@gmail.com>
2023-08-02 22:55:08 +0900

    更新

--- main.js
+++ main.js
@@ -99,14 +99,27 @@
 //   command = Number(prompt('Menu 1, 2, 3 or 0 to exit'));
 // }
 
-let command;
+// let command;
 
-do {
-  command = Number(prompt('Menu 1, 2, 3 or 0 to exit'));
-  if (command === 0) {
-    console.log('終了します');
-  } else {
-    console.log(`Menu ${command} processed.`);
+// do {
+//   command = Number(prompt('Menu 1, 2, 3 or 0 to exit'));
+//   if (command === 0) {
+//     console.log('終了します');
+//   } else {
+//     console.log(`Menu ${command} processed.`);
+//   }
+// } while (command !== 0);
+
+
+let amount = 100;
+
+for (let year = 1; year <= 30; year++) {
+  amount *= 1.05;
+  if (amount < 200) {
+    continue;
   }
-} while (command !== 0);
-
+  if (amount > 300) {
+    break;
+  }
+  console.log(`Year ${year}: ${amount}`);
+}
